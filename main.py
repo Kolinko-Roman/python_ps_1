eec5eda6d5ef30c63fce79eb8d7150772749b004
Kolinko Roman <118218201+Kolinko-Roman@users.noreply.github.com>
2025-02-23 16:23:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Завдання №1
+print("Hello, Python!")
+
+# Завадння №2 
+name = input("Print your name: ")
+print(f"Hi {name}!")
+
+# Завдання №3
+num_1 = float(input("Print firt number: "))
+num_2 = float(input("Print second number: "))
+print("Result: ",num_1 + num_2)
+
+
